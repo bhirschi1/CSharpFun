363e30e680c1d6698d4bfd351be7e501700c8d2a
Benjamin Hirschi <hirschiben@gmail.com>
2023-01-17 15:03:23 -0700

    Add project files.

---
+++ CSharpFun/PrintStuff.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFun
+{
+    class PrintStuff
+    {
+        private string language = "";
+        //Constructor
+        public PrintStuff (string temp)
+        {
+            language = temp;
+        }
+        public void PrintName(string name)
+        {
+            if (language == "EN")
+            {
+                Console.WriteLine("Hello, " + name);
+            }
+            if (language == "PT")
+            {
+                Console.WriteLine("Oi, " + name);
+            }
+        }
+        
+    }
+}
---
+++ CSharpFun/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpFun // name of the program
+{
+    class Program 
+    {
+        // Start
+        public static void Main(string[] args) // void is return type, string array named args are params
+        {
+            PrintStuff ps = new PrintStuff("PT"); //Instantiation; //Declaration
+            string ?userName = "";
+
+            Console.Write("Please enter your name: ");
+            userName = Console.ReadLine();
+
+            
+
+            ps.PrintName(userName);
+        }
+
+    }
+}
